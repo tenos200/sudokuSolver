04cadf3de15a537ddf20c1aa186630089c5be584
tenos200 <tim.enosedu@gmail.com>
2020-02-08 23:35:31 +0000

    test

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -2,14 +2,15 @@ board = [
     [7,8,0,4,0,0,1,2,0],
     [6,0,0,0,7,5,0,0,9],
     [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
     [0,0,1,0,5,0,9,3,0],
     [9,0,4,0,6,0,0,0,5],
     [0,7,0,3,0,0,0,1,2],
     [1,2,0,0,0,7,4,0,1],
-    [3,4,0,0,1,9,6,0,2],
     [0,4,9,2,0,6,0,0,7]
 
 ]
+
 def print_board(bo):
 
     for i in range(len(bo)):
@@ -26,8 +27,6 @@ def print_board(bo):
                 print(str(bo[i][j]) + " ", end ="") 
 
 
-print_board(board)
-
 def find_empty(boo):
     
     for i in range(len(bo)):
@@ -35,4 +34,36 @@ def find_empty(boo):
             if bo [i][j] == 0:
                 return(i, j) #returns row, column
             
-        
+            
+
+
+
+
+def valid(bo, num, pos):
+
+    #Checks row: loops through everysingle column in a given row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+    
+    #Checks column
+    for i in range(len(bo[0])):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+    
+    #Checks which box we are in 
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    #Loop throughs all 9 elements in box 
+    for i in range(box_y ^ 3, box_y*3 + 3):
+        for j in range(box_x ^ 3, box_x*3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+
+    return True 
+
+
+
+
+
